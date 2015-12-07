66135158ed1c93e755d1200d1f2305c7a822f2a9
Jyssica Baehr <jbaehr@localhost.localdomain>
2015-12-07 13:02:54 -0500

    Added a returning champion, so 3 players.

--- jeopardy.py
+++ jeopardy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 from prettytable import PrettyTable
 
+player1 = "Player1"
+player2 = "Player2"
+player3 = "Player3"
+
 topic1 = "America"
 topic2 = "Japan"
 topic3 = "Sports"
@@ -118,7 +122,10 @@ def initialTable():
 	print x.get_string(header=True, border=True)
 
 def instructions():
-	print("To choose a topic and score, enter exactly what is typed on Board (capatilization)\n")
+	print("To choose a topic and score, enter exactly what is typed on Board (capatilization)")
+	print("Returning champion selected topic first")
+	print("Buzz in to answer quesiton")
+	print(player1 + " use key r\n" + player2 + " use key a\n" + player2 + " use key l\n")
 
 def getQuestion(cat, amnt):
 	#if statements
@@ -255,8 +262,32 @@ def checkAnswer_p1(user_ans, ans, amnt):
 			score = score4
 		elif amnt == amount5:
 			score = score5
+	else:
+		print("That is... WRONG")
+		score = 0
 	global p1_score
-	p1_score = score
+	p1_score = p1_score + score
+
+def checkAnswer_p2(user_ans, ans, amnt):
+	
+
+	if user_ans == ans:
+		print("That is... CORRECT")
+		if amnt == amount1:
+			score = score1
+		elif amnt == amount2:
+			score = score2
+		elif amnt == amount3:
+			score = score3
+		elif amnt == amount4:
+			score = score4
+		elif amnt == amount5:
+			score = score5
+	else:
+		print("That is... WRONG")
+		score = 0
+	global p2_score
+	p2_score = p2_score + score
 
 def used_questions(cat, amnt):
 	#if statements
@@ -265,89 +296,107 @@ def used_questions(cat, amnt):
 			global amount1_1
 			amount1_1 = "--"
 		elif amnt == amount2:
-			global amount1_2
-			amount1_2 = "--"
-		elif amnt == amount3:
-			global amount1_3
-			amount1_3 = "--"
-		elif amnt == amount4:
-			global amount1_4
-			amount1_4 = "--"
-		elif amnt == amount5:
-			global amount1_5
-			amount1_5 = "--"
-	elif cat == topic2:
-		if amnt == amount1:
 			global amount2_1
 			amount2_1 = "--"
+		elif amnt == amount3:
+			global amount3_1
+			amount3_1 = "--"
+		elif amnt == amount4:
+			global amount4_1
+			amount4_1 = "--"
+		elif amnt == amount5:
+			global amount5_1
+			amount5_1 = "--"
+	elif cat == topic2:
+		if amnt == amount1:
+			global amount1_2
+			amount1_2 = "--"
 		elif amnt == amount2:
 			global amount2_2
 			amount2_2 = "--"
 		elif amnt == amount3:
-			global amount2_3
-			amount2_3 = "--"
-		elif amnt == amount4:
-			global amount2_4
-			amount2_4 = "--"
-		elif amnt == amount5:
-			global amount2_5
-			amount2_5 = "--"
-	elif cat == topic3:
-		if amnt == amount1:
-			global amount3_1
-			amount3_1 = "--"
-		elif amnt == amount2:
 			global amount3_2
 			amount3_2 = "--"
+		elif amnt == amount4:
+			global amount4_2
+			amount4_2 = "--"
+		elif amnt == amount5:
+			global amount5_2
+			amount5_2 = "--"
+	elif cat == topic3:
+		if amnt == amount1:
+			global amount1_3
+			amount1_3 = "--"
+		elif amnt == amount2:
+			global amount2_3
+			amount2_3 = "--"
 		elif amnt == amount3:
 			global amount3_3
 			amount3_3 = "--"
 		elif amnt == amount4:
-			global amount3_4
-			amount3_4 = "--"
-		elif amnt == amount5:
-			global amount3_5
-			amount3_5 = "--"
-	elif cat == topic4:
-		if amnt == amount1:
-			global amount4_1
-			amount4_1 = "--"
-		elif amnt == amount2:
-			global amount4_2
-			amount4_2 = "--"
-		elif amnt == amount3:
 			global amount4_3
 			amount4_3 = "--"
+		elif amnt == amount5:
+			global amount5_3
+			amount5_3 = "--"
+	elif cat == topic4:
+		if amnt == amount1:
+			global amount1_4
+			amount1_3 = "--"
+		elif amnt == amount2:
+			global amount2_4
+			amount2_4 = "--"
+		elif amnt == amount3:
+			global amount3_4
+			amount3_4 = "--"
 		elif amnt == amount4:
 			global amount4_4
 			amount4_4 = "--"
 		elif amnt == amount5:
-			global amount4_5
-			amount4_5 = "--"
-	elif cat == topic5:
-		if amnt == amount1:
-			global amount5_1
-			amount5_1 = "--"
-		elif amnt == amount2:
-			global amount5_2
-			amount5_2 = "--"
-		elif amnt == amount3:
-			global amount5_3
-			amount5_3 = "--"
-		elif amnt == amount4:
 			global amount5_4
 			amount5_4 = "--"
+	elif cat == topic5:
+		if amnt == amount1:
+			global amount1_5
+			amount1_5 = "--"
+		elif amnt == amount2:
+			global amount2_5
+			amount2_5 = "--"
+		elif amnt == amount3:
+			global amount3_5
+			amount3_5 = "--"
+		elif amnt == amount4:
+			global amount4_5
+			amount4_5 = "--"
 		elif amnt == amount5:
 			global amount5_5
 			amount5_5 = "--"
 	else:
 		return "You typed something incorrectly"
-	initialTable()
 
-def player1():
+def player2_turn():
 	initialTable()
-	print("JEOPARDY")
-	print("Player 1")
+	#print("JEOPARDY")
+	print(player2)
+	category = raw_input("Choose Category: ")
+	amount = raw_input("Choose Amount: ")
+	print("\nYou chose " + category + " at " + amount)
+	#call function, pass in category and amount, then it will return question
+	#function contains if statments	
+	question = getQuestion(category, amount)
+	answer = getAnswer(category, amount)
+	print(answer)
+	print("Your question is...  " + question)
+	user_answer = raw_input("Answer: ")
+	#function that checks answer
+	checkAnswer_p2(user_answer, answer, amount)
+	#function to cross of used questions on board
+	used_questions(category, amount)
+
+def player1_turn():
+	initialTable()
+	#print("JEOPARDY")
+	print(player1)
 	category = raw_input("Choose Category: ")
 	amount = raw_input("Choose Amount: ")
 	print("\nYou chose " + category + " at " + amount)
@@ -362,13 +411,24 @@ def player1():
 	checkAnswer_p1(user_answer, answer, amount)
 	#function to cross of used questions on board
 	used_questions(category, amount)
-	#now player2
-	#player2()
 	
 
 
 #program start executing
+player1 = raw_input("Returning Champion enter your name: ")
+player2 = raw_input("Player1 enter your name: ")
+player3 = raw_input("Player2 enter your name: ")
+
 instructions()
-player1()
+
+#for loop 25 times
+n = 25
+counter = 1
+#player1_turn()
+#key pressed for turn
+while counter < n:
+	player1_turn()
+	player2_turn()
+	counter += 1
 
 #program finishes executing
